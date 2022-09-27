8e54605dd4f7af97df800cafde5df403b21fa040
Navaneeth J <Navaneethjawahar333@gmail.com>
2022-09-27 11:54:31 -0400

    TEM-7 created Views and Forms for Signup

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,3 +1,55 @@
-from django.db import models
-
 # Create your models here.
+"""
+Since we are using Django default User models for authentication, Using models.py file for defining Signup and Login Forms.
+
+This code can be written in a new python file eg: forms.py
+"""
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+#Signup Form creation -> Will be Used in Views
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "User Name",
+            "class": "form-control",
+        }
+        )
+    )
+    studentID = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Student/Staff ID",
+            "class": "form-control",
+        }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "placeholder": "Email",
+            "class": "form-control",
+        }
+        )
+    )
+    pass1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Enter your Password",
+            "class": "form-control",
+        }
+        )
+    )
+    pass2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Re-enter your Password",
+            "class": "form-control",
+        }
+        )
+    )
+    class Meta:
+        model = User
+        fields = ('username', 'studentID', 'email', 'pass1', 'pass2')
+
+
+
+#Login Form creation -> will be used in views @sharath
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from xml.etree.ElementTree import QName
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import SignUpForm 
 
 # Create your views here.
+
+#This request comes from django HTTP request
+def user_signup_view(request): 
+    success = False
+    message = None
+    if request.method == "POST":
+        pass
+    else:
+        form = SignUpForm() 
+    
+    return render(request,"auth/signup.html", {"form": form, "msg": message, "success": success})
