fa748f23f1e6214496ba4ad8d258b6ddb9fb2a75
Navaneeth J <Navaneethjawahar333@gmail.com>
2022-10-12 11:20:45 -0400

    TEM-9 added buttons page and routing for it.

--- apps/home/views.py
+++ apps/home/views.py
@@ -12,3 +12,24 @@ def index(request):
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
+@login_required(login_url="/login/")
+def pages(request):
+    context = {}
+    try:
+        load_template = request.path.split('/')[-1]
+        if load_template == 'admin':
+            return HttpResponseRedirect(reverse('admin:index'))
+
+        context['segment'] = load_template
+        html_template = loader.get_template('home/' + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+        pass
+        #Insert 404 page if you have
+    except:
+        pass
+    #insert 500 page if you have
+
+
+
